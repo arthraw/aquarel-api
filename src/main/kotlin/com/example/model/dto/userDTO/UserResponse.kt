8d42a20853900c050cf50e2a07477829fa3004d9
arthraw <arthursallesandrade@gmail.com>
2024-01-28 17:25:33 -0300

    feat: User DTO

---
+++ src/main/kotlin/com/example/model/dto/userDTO/UserResponse.kt
@@ -0,0 +1,34 @@
+package com.example.model.dto.userDTO
+
+import com.example.utils.LocalDateSerializer
+import kotlinx.serialization.Serializable
+import java.time.LocalDate
+
+@Serializable
+data class User(
+    val id: String,
+    val username: String,
+    val email: String,
+    val password: String,
+    @Serializable(with = LocalDateSerializer::class)
+    val createAccountDate: LocalDate
+)
+@Serializable
+data class PatchUser(
+    val username: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+)
+@Serializable
+data class UserReturns(
+    val id: String,
+    val username: String,
+)
+@Serializable
+data class UserInsert(
+    val username: String,
+    val email: String,
+    val password: String,
+    @Serializable(with = LocalDateSerializer::class)
+    val createAccountDate: LocalDate
+)
