fee29ed7a4dd50a45dbccecbebac3b5ddf7de972
arthraw <arthursallesandrade@gmail.com>
2024-02-01 19:14:26 -0300

    feat: jwt server configuration

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,9 +1,14 @@
 package com.example
 
 import com.example.model.db.DbConfiguration
+import com.example.model.dto.userDTO.UserIdToken
+import com.example.model.dto.userDTO.UserLogin
 import com.example.plugins.*
+import com.example.utils.JwtConfig
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.plugins.contentnegotiation.*
 
 fun main(args: Array<String>) : Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -12,6 +17,11 @@ fun main(args: Array<String>) : Unit = io.ktor.server.netty.EngineMain.main(args
 fun Application.module() {
     install(ContentNegotiation) {
         json()
+    }
+    install(Authentication) {
+        jwt {
+            verifier(JwtConfig.verifier)
+        }
     }
     DbConfiguration.init()
     configureRouting()
