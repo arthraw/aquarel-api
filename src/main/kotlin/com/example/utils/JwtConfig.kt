6ce0ca864af58d362a923814b050c01c9d2474fc
arthraw <arthursallesandrade@gmail.com>
2024-02-27 21:58:59 -0300

    feat: jwt

--- src/main/kotlin/com/example/utils/JwtConfig.kt
+++ src/main/kotlin/com/example/utils/JwtConfig.kt
@@ -5,7 +5,6 @@ import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
 import com.example.model.dto.userDTO.UserLogin
 import io.github.cdimascio.dotenv.dotenv
-import java.util.*
 
 object JwtConfig {
 
@@ -21,11 +20,9 @@ object JwtConfig {
         .build()
 
     fun createToken(user : UserLogin) : String {
-        val expiration = Date(System.currentTimeMillis() + 3_600_000) // 1 hora
         return JWT.create()
             .withSubject(user.email)
             .withIssuer(issuer)
-            .withExpiresAt(expiration)
             .sign(Algorithm.HMAC256(secret))
     }
 }
