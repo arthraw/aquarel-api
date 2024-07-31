82617efcb6e9875ef5980977072996d7fce75e0b
arthraw <arthursallesandrade@gmail.com>
2024-07-30 23:14:18 -0300

    build: unnecessary dependencies deleted

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val exposed_version: String by project
-val mysql_version: String by project
-val dotenv_version: String by project
-val bcrypt_version: String by project
-val flyway_core_version: String by project
-val flyway_mysql_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val exposedVersion: String by project
+val mysqlVersion: String by project
+val dotenvVersion: String by project
+val bcryptVersion: String by project
+val flywayCoreVersion: String by project
+val flywayMysqlVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
@@ -38,47 +38,44 @@ repositories {
 dependencies {
     // DotEnv
     implementation(kotlin("stdlib"))
-    implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
+    implementation("io.github.cdimascio:dotenv-kotlin:$dotenvVersion")
 
     // Ktor
-    implementation("io.ktor:ktor-server-core:2.0.0")
+    implementation("io.ktor:ktor-server-core:2.3.0")
     implementation("io.ktor:ktor-server-netty:2.0.0")
-    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-
-    // jackson
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.5")
 
     //Gson
     implementation ("com.google.code.gson:gson:2.10.1")
 
     // Bcrypt
-    implementation("org.mindrot:jbcrypt:$bcrypt_version")
+    implementation("org.mindrot:jbcrypt:$bcryptVersion")
 
     // Exposed ORM
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
 
     // Flyway Migrations
-    implementation("org.flywaydb:flyway-mysql:$flyway_mysql_version")
-    implementation("org.flywaydb:flyway-core:$flyway_core_version")
+    implementation("org.flywaydb:flyway-mysql:$flywayMysqlVersion")
+    implementation("org.flywaydb:flyway-core:$flywayCoreVersion")
 
     // Database
-    implementation("mysql:mysql-connector-java:$mysql_version")
+    implementation("mysql:mysql-connector-java:$mysqlVersion")
 
     // Netty server
     implementation("io.ktor:ktor-server-netty-jvm")
 
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
     // Tests
-    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
     testImplementation("io.ktor:ktor-client-content-negotiation:2.1.1")
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
